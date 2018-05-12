a3ec958689f8e4f4cfa45c95c78fb97f05087ec2
V8-Bjoern <bjoern.internet@mailbox.org>
2018-05-12 20:46:52 +0200

    Solution of problem one and two (a)

--- exercise4/combine.C
+++ exercise4/combine.C
@@ -1,17 +1,29 @@
+#include <iostream>
 #include <cmath>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
 
 double background(double* x, double* pars);
 double signal(double* x, double* pars);
 double back_and_signal(double* x, double* pars);
+std::vector<double> residual(TH1D *exp1, TH1D *exp2);
 
 void combine()
 {
   // open file with data from experiment A
   TFile* fexpA = new TFile("exp_A.root", "READ");
-   
+
   // open file with data from experiment B
   TFile* fexpB = new TFile("exp_B.root", "READ");
-  
+  TFile* fexpC = new TFile("exp_C.root", "READ");
+  TFile* fexpD = new TFile("exp_D.root", "READ");
+
+
+  //New file with the results
+  TFile* results = new TFile("results.root","RECREATE");
+  if ( results->IsOpen() ) printf("File opened successfully\n");
   // list the file content
   fexpA->ls();
   fexpB->ls();
@@ -19,13 +31,83 @@ void combine()
   // get the histogram containing the data
   TH1D *expA = (TH1D*)fexpA->Get("exp_A");
   TH1D *expB = (TH1D*)fexpB->Get("exp_B");
+  TH1D *expC = (TH1D*)fexpC->Get("exp_C");
+  TH1D *expD = (TH1D*)fexpD->Get("exp_D");
+  /*
+  Probelem 1
+  */
+  std::vector<double> residualAB=residual(expA, expB);
+  std::vector<double> residualAC=residual(expA, expC);
+  std::vector<double> residualAD=residual(expA, expD);
+  std::vector<double> residualBC=residual(expB, expC);
+  std::vector<double> residualBD=residual(expB, expD);
+  std::vector<double> residualCD=residual(expC, expD);
 
-  // draw it 
+ //std::vector<std::vector<double>> residuals = {residualAB,residualAC,residualAD,residualBC,residualBD,residualCD};
+
+  TH1D* HResidualAB= new TH1D("HResidualAB", "HResidualAB", 10, -1000, 1000);
+  TH1D* HResidualAC= new TH1D("HResidualAC", "HResidualAC", 10, -1000, 1000);
+  TH1D* HResidualAD= new TH1D("HResidualAD", "HResidualAD", 10, -1000, 1000);
+  TH1D* HResidualBC= new TH1D("HResidualBC", "HResidualBC", 10, -1000, 1000);
+  TH1D* HResidualBD= new TH1D("HResidualBD", "HResidualBD", 10, -1000, 1000);
+  TH1D* HResidualCD= new TH1D("HResidualCD", "HResidualCD", 10, -1000, 1000);
+
+  //std:vector<TH1D*> ResidualHistos = {HResidualAB,HResidualAC,HResidualAD,HResidualAD,HResidualBC,HResidualBD,HResidualCD};
+
+  for(int i = 0;i<residualAB.size();i++) {
+      HResidualAB->Fill(residualAB[i]);
+      HResidualAC->Fill(residualAC[i]);
+      HResidualAD->Fill(residualAD[i]);
+      HResidualBC->Fill(residualBC[i]);
+      HResidualBD->Fill(residualBD[i]);
+      HResidualCD->Fill(residualCD[i]);
+  }
+  std::ofstream fout1("solutionOfProblem1.txt");
+
+  fout1 << "Name    Mean    RMS" << endl;
+  fout1 << "AB    " <<  HResidualAB->GetMean() << "   "<< HResidualAB->GetRMS() << endl;
+  fout1 << "AC    " <<  HResidualAC->GetMean() << "   "<< HResidualAC->GetRMS() << endl;
+  fout1 << "AD    " <<  HResidualAD->GetMean() << "   "<< HResidualAD->GetRMS() << endl;
+  fout1 << "BC    " <<  HResidualBC->GetMean() << "   "<< HResidualBC->GetRMS() << endl;
+  fout1 << "BD    " <<  HResidualBD->GetMean() << "   "<< HResidualBD->GetRMS() << endl;
+  fout1 << "CD    " <<  HResidualCD->GetMean() << "   "<< HResidualCD->GetRMS() << endl;
+  fout1.close();
+
+
+
+  /*
+  Probelem 2
+  */
+  std::ofstream fout2("solutionOfProblem2.txt");
+  HResidualAB->Fit("gaus");
+  TF1 *fit1 = HResidualAB->GetFunction("gaus");
+  fout2 <<"GetChisquareAB: " << fit1->GetChisquare() << endl;
+  HResidualAC->Fit("gaus");
+  TF1 *fit2 = HResidualAC->GetFunction("gaus");
+  fout2 <<"GetChisquareAC: " << fit2->GetChisquare() << endl;
+  HResidualAD->Fit("gaus");
+  TF1 *fit3 = HResidualAD->GetFunction("gaus");
+  fout2 <<"GetChisquareAD: " << fit3->GetChisquare() << endl;
+  HResidualBC->Fit("gaus");
+  TF1 *fit4 = HResidualBC->GetFunction("gaus");
+  fout2 <<"GetChisquareBC: " << fit4->GetChisquare() << endl;
+  HResidualBD->Fit("gaus");
+  TF1 *fit5 = HResidualBD->GetFunction("gaus");
+  fout2 <<"GetChisquareBD: " << fit5->GetChisquare() << endl;
+  HResidualCD->Fit("gaus");
+  TF1 *fit6 = HResidualCD->GetFunction("gaus");
+  fout2 <<"GetChisquareCD: " << fit6->GetChisquare() << endl;
+
+
+  fout2.close();
+
+  // draw it
   expA->Draw("E1");
+
 
   // this is how you can get the number of bins:
   cout << "number of bins = " << expA->GetNbinsX() << endl;
-  
+
   // here is the measurement in some bin with error:
   cout << "measurement in bin 27: " << expA->GetBinContent(27) << " +- " << expA->GetBinError(27) << endl;
 
@@ -46,7 +128,7 @@ void combine()
   pars[4] = 0.001;
   pars[5] = 160;
   pars[6] = 20;
-  
+
   TF1* bf = new TF1("background", background, 20, 300, 4);
   bf->SetParameters(bpars);
   bf->SetLineColor(kBlue+1);
@@ -54,6 +136,15 @@ void combine()
 
   bf->Draw("same");
 
+
+  results->WriteTObject(HResidualAB);
+  results->WriteTObject(HResidualAC);
+  results->WriteTObject(HResidualAD);
+  results->WriteTObject(HResidualBC);
+  results->WriteTObject(HResidualBD);
+  results->WriteTObject(HResidualCD);
+
+  results->Close();
 }
 
 // define functions
@@ -66,7 +157,7 @@ double background(double* x, double* pars)
   double delta = pars[3];
 
   y = alpha + beta*x[0] + gamma*exp(delta*x[0]);
-  
+
   return y;
 }
 
@@ -84,22 +175,22 @@ double signal(double* x, double* pars)
 
 double back_and_signal(double* x, double* pars)
 {
-  return background(x,pars) + signal(x,pars+4);  
+  return background(x,pars) + signal(x,pars+4);
 }
 
 std::vector<double> residual(TH1D *exp1, TH1D *exp2)
 {
-	std::vector *result = new std::vector();
+	std::vector<double> result;
 	double chi;
 	double NaNb;
 	double sigma;
-	for(int i=0; i < expA->GetNbins1(); i++)
+	for(int i=0; i < exp1->GetNbinsX(); i++)
 	{
-		NaNb = exp1->GetBinContent(i)-exp2->GetBinContent(i)); 
-		sigma = pow(exp1->GetBinError(i),2.)+pow(exp2->GetBinError(i),2.)
+		NaNb = (exp1->GetBinContent(i)-exp2->GetBinContent(i));
+		sigma = pow(exp1->GetBinError(i),2.)+pow(exp2->GetBinError(i),2.);
 		chi = NaNb/sigma;
-		result->push_back(chi);
+		result.push_back(chi);
 	}
 	return result;
-	
+
 }
